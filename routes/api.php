55661547cd9d71fdda3138432a52716f3aa9fb21
savinduamalka <savinduamalka@gmail.com>
2024-12-09 22:17:22 +0530

    defined the routes

---
+++ routes/api.php
@@ -0,0 +1,54 @@
+<?php
+
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Auth\RegisteredUserController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ProductController;
+use App\Http\Middleware\EnsureUserIsAdmin;
+use App\Http\Resources\UserResource;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+use App\Http\Controllers\AdminDashboardController;
+
+// Authentication Routes
+Route::post('/register', [RegisteredUserController::class, 'store']);
+Route::post('/login', [AuthenticatedSessionController::class, 'store']);
+
+// Product Routes
+Route::prefix('products')->group(function () {
+    Route::get('/', [ProductController::class, 'index']);
+    Route::get('/{product}', [ProductController::class, 'show'])
+        ->missing(fn() => response()->json(['message' => 'Product not found'], 404));
+    Route::get('/category/{categoryId}', [ProductController::class, 'byCategory'])
+        ->missing(fn() => response()->json(['message' => 'Category not found'], 404));
+});
+
+
+// Category Routes
+Route::prefix('categories')->group(function () {
+    Route::get('/', [CategoryController::class, 'index']);
+});
+
+
+Route::get('/featured-products', [ProductController::class, 'featured']);
+
+//  Protected Routes
+Route::middleware('auth:sanctum')->group(function () {
+    // User Routes
+    Route::middleware('auth:sanctum')->get('/user', [RegisteredUserController::class, 'index']);
+    Route::middleware('auth:sanctum')->delete('/user/{id}', [RegisteredUserController::class, 'destroy']);
+
+   
+    Route::post('/logout', [AuthenticatedSessionController::class, 'destroy']);
+
+    // Admin Routes
+    Route::get('/admin/stats', [AdminDashboardController::class, 'getStats']);
+
+     //create category
+    Route::post('/category', [CategoryController::class, 'store']);
+    Route::middleware(EnsureUserIsAdmin::class)->prefix('products')->group(function () {
+        Route::post('/', [ProductController::class, 'store']);
+        Route::put('/{product}', [ProductController::class, 'update']);
+        Route::delete('/{product}', [ProductController::class, 'destroy']);
+    });
+});
