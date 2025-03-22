50e2d7782eb829ac9c5ccb46d31097c17c025bcc
savinduamalka <savinduamalka@gmail.com>
2025-03-22 10:58:54 +0530

    disable csrf

---
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
+
+class VerifyCsrfToken extends Middleware
+{
+    /**
+     * The URIs that should be excluded from CSRF verification.
+     *
+     * @var array<int, string>
+     */
+    protected $except = [
+        '*',
+        'register',
+        'login',
+        'logout',
+        'cart',
+        'cart/*',
+        'orders',
+        'orders/*',
+        'category',
+        'category/*',
+        'products',
+        'products/*',
+        'subscribe',
+        'emails',
+        'user/*',
+        'admin/stats',
+        'test',
+    ];
+}
